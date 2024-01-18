b1e197f178c3583bc38bc21b40f88a29bbc2946e
Phan Hawk <97074495+hawkphan@users.noreply.github.com>
2024-01-18 08:37:58 +0700

    Login Added

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Extensions;
 using Application.Activities;
 using Application.Core;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -10,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -32,8 +35,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context,userManager);
 
 }
 catch (Exception ex)
