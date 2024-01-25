c93d27076f2eb6f74d9d781a3968390bedeb7cde
Trieu Le <trieu.le.cit19@eiu.edu.vn>
2024-01-25 09:23:52 +0700

    remove container tag in app.tsx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,16 +2,13 @@ import { Fragment } from "react";
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../containers/Navbar";
-import { Container } from "@mui/material";
 
 // Code Start Here
 function App() {
   return (
     <Fragment>
       <Navbar />
-      <Container style={{fontFamily: 'Roboto'}}>
-        <Outlet />
-      </Container>
+      <Outlet />
     </Fragment>
   );
 }
