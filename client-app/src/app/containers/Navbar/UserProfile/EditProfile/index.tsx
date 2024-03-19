84e3518abcdd1ca880995dac473404ded62b3a17
Kiet2022 <108647997+Kiet2022@users.noreply.github.com>
2024-03-19 09:51:54 +0700

    Create EditProfile folder

--- client-app/src/app/containers/Navbar/UserProfile/EditProfile/index.tsx
+++ client-app/src/app/containers/Navbar/UserProfile/EditProfile/index.tsx
@@ -1,5 +1,12 @@
-const EditProfile = () => {
-    return <></>
-}
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/joy/styles';
+import App from './App';
 
-export default EditProfile;
+ReactDOM.createRoot(document.querySelector("#root")!).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
