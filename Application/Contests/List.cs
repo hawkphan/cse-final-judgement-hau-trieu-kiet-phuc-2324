6a1c94d9941d99ee9cb12f8ee8373574db418dfd
Trieu Le <trieu.le.cit19@eiu.edu.vn>
2024-03-19 17:22:58 +0700

    feat: clone contest crud

---
+++ Application/Contests/List.cs
@@ -0,0 +1,37 @@
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Domain;
+using Domain.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Contests
+{
+    public class List
+    {
+        public class Query : IRequest<Result<PagedList<ContestDto>>>
+        {
+            public PagingParams Params { get; set; }
+            public Guid? UserId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<PagedList<ContestDto>>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<PagedList<ContestDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+               throw new NotImplementedException();
+            }
+        }
+    }
+}
